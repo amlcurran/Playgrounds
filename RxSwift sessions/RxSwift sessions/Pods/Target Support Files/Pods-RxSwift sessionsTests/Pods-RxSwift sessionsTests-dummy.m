15ba65e801add28350ec6df0defd62483a69701e
Alex Curran <aml.curran@gmail.com>
2016-11-23 15:49:05 +0100

    Initial commit of Rx 1

---
+++ RxSwift sessions/RxSwift sessions/Pods/Target Support Files/Pods-RxSwift sessionsTests/Pods-RxSwift sessionsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxSwift_sessionsTests : NSObject
+@end
+@implementation PodsDummy_Pods_RxSwift_sessionsTests
+@end
---
+++ RxSwift sessions/RxSwift sessions/RxSwift sessionsTests/MapTests.swift
@@ -0,0 +1,19 @@
+//
+//  RxSwift_sessionsTests.swift
+//  RxSwift sessionsTests
+//
+//  Created by Alex on 15/11/2016.
+//  Copyright © 2016 amlcurran. All rights reserved.
+//
+
+import XCTest
+@testable import RxSwift_sessions
+
+class RxSwift_sessionsTests: XCTestCase {
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
